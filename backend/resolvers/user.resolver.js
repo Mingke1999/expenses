c66d020bef9bc000e14882792d28c96d6964d973
mingke-utas <95258603+Mingke1999@users.noreply.github.com>
2024-06-08 23:59:18 +1000

    user login/logout

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,17 +1,96 @@
 import { users } from "../dummyData/data.js";
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs'
 
 const userResolver = {
     Query:{
         //fetch all users
-        users:(_,args,{req, res}) =>{
-            return users
+        authUser:async(_,args,context) =>{
+            try{
+                const user = await context.getUser()
+                return user;
+            }catch(err){
+                console.error("Error in authticating user",err);
+                throw new Error(err.message || "Internal Server Error");
+            }
         },
         //select user by userId ->from resolver user(userId:ID!):User
-        user:(_, args) =>{
-            return users.find((user) =>user._id === args.userId)
+        user:async(_, args) =>{
+           try{
+            const user = await User.findById(args.userId)
+            return user;
+           }catch(err){
+                console.error("Error in fetching user",err);
+                throw new Error(err.message || "Internal Server Error");
+           }
         }
     },
-    Mutation:{}
+    Mutation:{
+        // input SignUpInput{
+        //     username:String!
+        //     name:String!
+        //     password:String!
+        //     gender:String!
+        // }
+        signup:async(_,args,context) =>{
+            try{
+                const {username, name, password, gender} = args
+                if(!username || !name || !password || !gender){
+                    throw new Error("signup parameter missing");
+                }
+                const exsitingUser = await User.findOne({username});
+                if(exsitingUser){
+                    throw new Error("The user existing");
+                }
+                const salt =  await bcrypt.genSalt(10)
+                const hashedPassword = await bcrypt.hash(password, salt);
+                const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+                const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}` ;
+
+                const newUser = new User({
+                    username,
+                    name, 
+                    password:hashedPassword,
+                    gender,
+                    profilePicture:gender === "male" ? boyProfilePic : girlProfilePic
+                })
+                await newUser.save();
+                await context.login(newUser);
+
+                return newUser;
+            }catch(err){
+                console.error("Error in signup",err);
+                throw new Error(err.message || "Internal Server Error");
+            }
+        },
+        login:async(_,args,context)=>{
+            try{
+                const {username, password} = args;
+                const {user} = await context.authenticate("graphql-local",{username, password})
+
+                await context.login(user);
+
+                return user;
+            }catch(err){
+                console.error("Error in login", err);
+                throw new Error(err.message || "Internal Server Error");
+            }
+        },
+        logout:async(_,args,context)=>{
+            try{
+                await context.logout();
+                req.session.destroy((err)=>{
+                    if(err) throw err
+                })
+                res.clearCookie("connect.sid");
+                return {message:"Logged out"};
+            }catch(err){
+                console.error("Error in logout", err);
+                throw new Error(err.message || "Internal Server Error");
+            }
+        }
+        
+    }
 }
 
 export default userResolver;
